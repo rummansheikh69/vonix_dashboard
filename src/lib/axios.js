3acbb97eab5c1c5652a4f4936f39f67f2181e669
rummansheikh69 <rummansheikh2004@gmail.com>
2026-02-24 09:58:50 +0600

    fix: update axios baseURL to production endpoint

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://onex-backend.vercel.app/api",
   withCredentials: true,
 });
